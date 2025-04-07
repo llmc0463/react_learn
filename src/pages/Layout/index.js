920c00ba74796017ea8771afc85d49231a6c4e75
李孟宸 <limengchen@limengchendeMacBook-Air.local>
2025-04-07 21:48:04 +0800

    tabBar切换路由功能实现

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,25 +1,81 @@
-import { Button} from 'antd-mobile'
-import { Outlet } from "react-router-dom"
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { getBillList } from '@/store/modules/billStore'
+// import { Button} from 'antd-mobile'
+// import { Outlet } from "react-router-dom"
+// import { useEffect } from 'react'
+// import { useDispatch } from 'react-redux'
+// import { getBillList } from '@/store/modules/billStore'
 
-const Layout = ()=>{
-	const dispatch = useDispatch()  
-	useEffect(()=>{
-		dispatch(getBillList())
-	},[dispatch])
-    return(
-      <div>
-        <Outlet />
-        我是layout
-		{/*测试全局生效样式*/}
-		<Button color="primary">测试全局</Button>
-		{/*测试局部生效样式*/}
-		<div className='purple '>
-		<Button color="primary">测试局部</Button>
-		</div>
-      </div>
-    )
+// const Layout = ()=>{
+// 	const dispatch = useDispatch()
+// 	useEffect(()=>{
+// 		dispatch(getBillList())
+// 	},[dispatch])
+//     return(
+//       <div>
+//         <Outlet />
+//         我是layout
+// 		{/*测试全局生效样式*/}
+// 		<Button color="primary">测试全局</Button>
+// 		{/*测试局部生效样式*/}
+// 		<div className='purple '>
+// 		<Button color="primary">测试局部</Button>
+// 		</div>
+//       </div>
+//     )
+// }
+// export default Layout
+
+import { TabBar } from "antd-mobile"
+import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { getBillList } from "@/store/modules/billStore"
+import './index.scss'
+import {
+		BillOutline,
+		CalculatorOutline,
+		AddCircleOutline,
+		} from 'antd-mobile-icons'
+	const tabs = [
+		{
+		key: '/month',
+		title: '⽉度账单',
+		icon: <BillOutline />,
+		},
+		{
+		key: '/new',
+		title: '记账',
+		icon: <AddCircleOutline />,
+		},
+		{
+		key: '/year',
+		title: '年度账单',
+		icon: <CalculatorOutline />,
+		},
+	]
+	const Layout = () => {
+		const dispatch = useDispatch()
+		useEffect(() => {
+			dispatch(getBillList())
+ 		}, [dispatch])
+		// 切换菜单跳转路由
+		const navigate = useNavigate()
+		const switchRouter = (path) => {
+			console.log(path)
+			navigate(path)
+		}
+		return (
+			<div className="layout">
+				<div className="container">
+				<Outlet />
+				</div>
+				<div className="footer">
+					<TabBar onChange={switchRouter}>
+						{tabs.map(item => (
+					<TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+						))}
+					</TabBar>
+				</div>
+			</div>
+ )
 }
 export default Layout
